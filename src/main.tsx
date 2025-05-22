404c53797392c0562e6a0e094db901b149c83bec
Hamza Ali <plomenet@gmail.com>
2025-05-22 22:32:24 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/flemonade" element={<Index />} />
         <Route path="/theme" element={<Theme />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
