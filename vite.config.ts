0e38f9224a3adfc3f5889a2fa57575f5f155e0df
Hamza Ali <plomenet@gmail.com>
2025-05-22 22:30:18 +0800

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flemonade/',
+  base: process.env.BASE_URL || '/flemonade/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
