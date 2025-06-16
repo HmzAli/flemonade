51143d26a513b7505cfbce73cbffe06e6362a2b6
Hamza Ali <plomenet@gmail.com>
2025-06-16 13:53:54 +0300

    test

--- src/components/MainBanner/index.tsx
+++ src/components/MainBanner/index.tsx
@@ -7,7 +7,7 @@ const MainBanner = () => {
       <div className="container">
         <div className="tagline" data-content-id="tagline">
            <h1 className="tagline__title" data-aos="fade-in" data-aos-duration="500">Premium Taste. Made for Everyone</h1>
-           <p className="tagline__description" data-aos="fade-in" data-aos-duration="800">Fresh. Bold. Handmade. Zesty AF!</p>
+           <p className="tagline__description" data-aos="fade-in" data-aos-duration="800">Fresh. Bold. Handmade. Zesty AF</p>
         </div>
 
         <Cta text="order on whatsapp" link="https://wa.me/+601111474404" variant="secondary" icon="whatsapp" />
